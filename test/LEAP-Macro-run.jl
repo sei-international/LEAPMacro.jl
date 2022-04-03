1b72141c40ec221b3bcc7679532cb05688aa7698
Eric Kemp-Benedict <eric.kemp-benedict@sei.org>
2022-04-03 08:49:49 -0400

    Tweaks to test model while working through LEAP exercise

--- test/LEAP-Macro-run.jl
+++ test/LEAP-Macro-run.jl
@@ -3,7 +3,7 @@ using LEAPMacro
 curr_working_dir = pwd()
 cd(@__DIR__)
 
-println("Running Default...")
+println("Running Baseline...")
 LEAPMacro.run("LEAPMacro_params.yml", dump_err_stack = false)
 
 cd(curr_working_dir)
