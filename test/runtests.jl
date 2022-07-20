2f6db877ea99abff32dbee03f46bc56f2de120c9
Eric Kemp-Benedict <eric.kemp-benedict@sei.org>
2022-07-20 18:56:28 -0400

    Expanded tests to run basic and full, both with and without optional extensions

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,9 @@ cd(@__DIR__)
 
 @testset "LEAPMacro.run" begin
 	@test LEAPMacro.run("LEAPMacro_params.yml", dump_err_stack = true, include_energy_sectors = true) == 0
+	@test LEAPMacro.run("LEAPMacro_params.yml", dump_err_stack = true, include_energy_sectors = false) == 0
+	@test LEAPMacro.run("LEAPMacro_params_all_options.yml", dump_err_stack = true, include_energy_sectors = true) == 0
+	@test LEAPMacro.run("LEAPMacro_params_all_options.yml", dump_err_stack = true, include_energy_sectors = false) == 0
 end
 
 cd(curr_working_dir)
