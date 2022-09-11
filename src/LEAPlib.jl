d46171f19be10ee876c8d7cb11515e9fa06a087e
Eric Kemp-Benedict <eric.kemp-benedict@sei.org>
2022-09-11 09:41:23 -0400

    Adding refresh calls to calculate_leap

--- src/LEAPlib.jl
+++ src/LEAPlib.jl
@@ -225,6 +225,9 @@ function calculate_leap(scen_name::AbstractString)
     # connects program to LEAP
     LEAP = connect_to_leap()
     try
+        LEAP.RefreshBranches()
+        LEAP.RefreshResources()
+        LEAP.Refresh()
         LEAP.ForceCalculation()
         LEAP.Scenario(scen_name).ResultsShown = true
         LEAP.Calculate(false) # This sets RunWEAP = false
