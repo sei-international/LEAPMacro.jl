582900654a1317c63cda43b103aa0408c4cd4907
Eric Kemp-Benedict <eric.kemp-benedict@sei.org>
2022-10-05 12:00:12 -0400

    Added (but commented out) version id testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -25,6 +25,13 @@ cd(@__DIR__)
 						continue_if_error = false,
 						load_leap_first = false,
 						include_energy_sectors = false) == 0
+	# @test LEAPMacro.run("LEAPMacro_params.yml",
+	# 					dump_err_stack = true,
+	# 					continue_if_error = false,
+	# 					load_leap_first = true,
+	# 					get_results_from_leap_version = "CALCULATED TEST",
+	# 					only_push_leap_results = true,
+	# 					include_energy_sectors = false) == 0
 end
 
 cd(curr_working_dir)
